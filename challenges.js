83379a932b046f6cd124386009dc6aa84812fbb1
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2021-05-04 23:27:44 +0300

    Coding Challenge #2

--- challenges.js
+++ challenges.js
@@ -20,23 +20,23 @@ m tall
 */
 
 // 1
-//const marksWeight = 78;
-//const marksHeight = 1.69;
+/* const marksWeight = 78;
+const marksHeight = 1.69;
 
-//const jhonsWeight = 92;
-//const jhonsHeight = 1.95;
+const jhonsWeight = 92;
+const jhonsHeight = 1.95; */
 
-//const marksWeight = 95;
-//const marksHeight = 1.88;
+const marksWeight = 95;
+const marksHeight = 1.88;
 
-//const jhonsWeight = 85;
-//const jhonsHeight = 1.76;
+const jhonsWeight = 85;
+const jhonsHeight = 1.76;
 
 
 //2
-//const marksBmi = marksWeight / marksHeight ** 2;
+const marksBmi = marksWeight / marksHeight ** 2;
 //const marksBmi = marksWeight / (marksHeight * marksHeight);
-//const jhonsBmi = jhonsWeight / jhonsHeight ** 2;
+const jhonsBmi = jhonsWeight / jhonsHeight ** 2;
 //const jhonsBmi = marksWeight / (jhonsHeight * jhonsHeight);
 
 //3
@@ -47,3 +47,30 @@ m tall
 //console.log(jhonsBmi);
 //console.log(marksBmi, jhonsBmi, markHigherBMI);
 //console.log(markHigherBMI);
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and 
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message 
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 
+GOOD LUCK  */
+
+
+
+if(marksBmi > jhonsBmi) {
+    console.log(`Mark's BMI (${marksBmi}) is higher than John's!`)
+} else {
+    console.log(`John's (${jhonsBmi}) BMI is higher than Mark's!`)
+}
+
+
+
+
+
+
