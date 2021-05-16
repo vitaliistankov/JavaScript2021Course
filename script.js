ea0256a975f9e5d33d7327e151e963f9594d8ac3
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2021-05-16 21:06:10 +0300

    The switch Statement

--- script.js
+++ script.js
@@ -223,6 +223,78 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
       console.log('Someone else should drive...')
 } */ 
 
+///////////////////////////////////////////////////////
+
+// 26. The switch Statement
+
+const day = 'thursday';
+
+switch(day){
+      case 'monday': // day === 'monaday'
+            console.log('Plan course structure');
+            console.log('Go to coding meetup');
+            break;
+      case 'turesday':
+            console.log('prepare theory videous');
+      case 'wednesday':
+      case 'thursday':
+            console.log('Write code examples');
+            break;
+      case 'friday':
+            console.log('Record videos');
+            break;
+      case 'saturday':
+      case 'sunday':
+            console.log('Enjoy the weekend :D');
+            break;
+      default:
+            console.log('Not a valid day!');
+}
 
 
+if(day === 'monday') {
+      console.log('Plan course structure. Go to coding meetup')
+}else if(day === 'turesday') {
+            console.log('prepare theory videous'); 
+}else if (day === 'wednesday' || day === 'thursday') {
+      console.log('Write code examples')
+}else if (day ==='friday') {
+      console.log('Record videos');
+}else if (day === 'saturday' || day === 'sunday') {
+      console.log('Enjoy the weekend :D');
+}else{
+console.log('Not a valid day!');
+}
 
+
+if(day === 'monday') {
+      console.log('Plan course structure. Go to coding meetup')
+}else if(day === 'turesday') {
+            console.log('prepare theory videous'); 
+}else if (day === 'wednesday' || day === 'thursday') {
+      console.log('Write code examples')
+}else if (day ==='friday') {
+      console.log('Record videos');
+}else if (day === 'saturday' || day === 'sunday') {
+      console.log('Enjoy the weekend :D');
+}else{
+console.log('Not a valid day!');
+}
+
+const day = 'friday';
+
+if(day === 'monday') {
+      console.log('You can hold your head')
+}else if(day === 'turesday' ||  day === 'wednesday') {
+            console.log('Stay in bed'); 
+}else if (day === 'thursday') {
+      console.log('Watch the walls instead')
+}else if (day ==='friday') {
+      console.log('Im in love');
+}else if (day === 'saturday') {
+      console.log('Wait');
+}else if (day === 'sunday') {
+      console.log('Always comes too late');
+}else{
+console.log('(Oh)Do-do, do-do, do-do, do, Do-do, do-do, do-do, do(Oh, oh, whoa, whoa)!');
+}
