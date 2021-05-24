d4c4bb9192d65e7056214ea94046c7d997fe8d69
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2021-05-24 22:09:22 +0300

    Function Declarations vs. Expressions

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 'use strict';
 
-let hasDriversLicense = false;
+/* let hasDriversLicense = false;
 const passTest = true;
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+if (hasDriversLicense) console.log('I can drive :D'); */
+
+/* function logger() {
+    console.log('My name is Vitalii');
+}
+
+//calling / running / invokong function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23'); */
+
+
+
+//Function declaration
+const age1 = calcAge1(1971);
+
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+
+//Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1971);
+
+console.log(age1, age2);
+
