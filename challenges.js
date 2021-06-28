b10d94b21fb25947c7e8e490c313941824fb109b
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2021-06-28 16:38:52 +0300

    38. Coding Challenge #1 Solved

--- challenges.js
+++ challenges.js
@@ -149,11 +149,11 @@ else if (dolphinsAverageScore === koalasAverageScore && dolphinsAverageScore >= 
 
 /* Coding Challenge #4
 Steven wants to build a very simple tip calculator for whenever he goes eating in a
-restaurant.In his country, it's usual to tip 15% if the bill value is between 50 and 
+restaurant.In his country, it's usual to tip 15% if the bill value is between 50 and
 300. If the value is different, the tip is 20 %.
 Your tasks:
-1. Calculate the tip, depending on the bill value.Create a variable called 'tip' for 
-this. It's not allowed to use an if/else statement � (If it's easier for you, you can 
+1. Calculate the tip, depending on the bill value.Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
 start with an if/else statement, and then try to convert it to a ternary operator!)
 2. Print a string to the console containing the bill value, the tip, and the final value
 (bill + tip).Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
@@ -190,9 +190,98 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tot
     console.log(`The bill was ${bill}, the tip was ${bill}*0.15, and the total value ${bill} + ${tip}`);
  */
 
-const bill = 275;
+/* const bill = 275;
 
 const tip = bill <= 300 && bill >= 50 ? bill * .15 :
     bill * 0.2;
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`); */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//JavaScript Fundamentals – Part 2
+
+/* Coding Challenge #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+    gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated(so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+    Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+    Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores */
+
+
+
+
+
+/* const calcAverage = (score1, score2, score3) => {
+
+    return (score1 + score2 + score3) / 3;
+};
+
+const avgDolhins = calcAverage(85, 54, 49);
+const avgKoalas = calcAverage(23, 34, 27);
+
+console.log(avgDolhins);
+console.log(avgKoalas);
+
+
+function checkWinner(avgDolhins, avgKoalas) {
+
+
+    if (avgDolhins > avgKoalas) {
+
+        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+
+    } */
+
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+//Test1
+let scoreDolphins = calcAverage(44, 23, 74);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+
+const checkWinner = function (avgDolhins, avgKoalas) {
+
+    if (avgDolhins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolhins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+    } else {
+        console.log('No team wins')
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+// Test 2
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+
 
